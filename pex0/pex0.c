74c896fbfb1b017bfd8db62ae6b1b47b1aedc0ae
Infamy <infamy@unyx.io>
2019-08-12 14:16:30 -0600

    drawCat and drawTree

--- pex0/pex0.c
+++ pex0/pex0.c
@@ -4,28 +4,27 @@
  */
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
-/***
- *  Function prototypes
- */
-// TODO 3.2 - Exercise 3 - Pythagorean Theorem
-double pythagorean(int, int);
+void drawTree(void);
+void drawCat(void);
 
 int main() {
-
-    // TODO 3.3 - Exercise 3 - Pythagorean Theorem
-    int a, b;
-    printf("Enter lengths of sides a and b: ");
-    scanf("%d", &a);
-    scanf("%d", &b);
-    printf("The length of the hypotenuse is %.6lf.", pythagorean(a, b));
-    // Enter lengths of sides a and b: 3 4
-    // The length of the hypotenuse is 5.000000.
-
-    return 0;
+    drawTree();
+    drawCat();
 }
 
-// TODO 3.1 - Exercise 3 - Pythagorean Theorem
-double pythagorean(int a, int b) {
-    return sqrt(pow(a, 2) + pow(b, 2));
+void drawTree() {
+    printf("   *   \n");
+    printf("  ***  \n");
+    printf(" ***** \n");
+    printf("*******\n");
+    printf("  ***  \n");
+}
+
+void drawCat() {
+    printf("/\\   /\\\n");
+    printf("  o o\n");
+    printf(" =   =\n");
+    printf("  ---\n");
 }
