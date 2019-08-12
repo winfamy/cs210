a1671c9dc56a07b10e8b0dac9046c448f078b1af
Infamy <infamy@unyx.io>
2019-08-12 14:21:49 -0600

    finished coursePercentage

--- pex0/pex0.c
+++ pex0/pex0.c
@@ -8,10 +8,22 @@
 
 void drawTree(void);
 void drawCat(void);
+double coursePercentage(double, double, double);
 
 int main() {
     drawTree();
     drawCat();
+
+    int h;
+    int m;
+    int f;
+    printf("Homework (x / 80): ");
+    scanf("%d", &h);
+    printf("Midterm (x / 40): ");
+    scanf("%d", &m);
+    printf("Final (x / 70): ");
+    scanf("%d", &f);
+    printf("Total percentage: %.2lf%%", coursePercentage(h/80.0, m/40.0, f/70.0));
 }
 
 void drawTree() {
@@ -19,12 +31,16 @@ void drawTree() {
     printf("  ***  \n");
     printf(" ***** \n");
     printf("*******\n");
-    printf("  ***  \n");
+    printf("  ***  \n\n");
 }
 
 void drawCat() {
     printf("/\\   /\\\n");
     printf("  o o\n");
     printf(" =   =\n");
-    printf("  ---\n");
+    printf("  ---\n\n");
+}
+
+double coursePercentage(double h, double m, double f) {
+    return (h * .2 + m * .3 + f * .5) * 100;
 }
